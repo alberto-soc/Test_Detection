86898eeac06aa518fe226cc5519904f39e4a68e9
Alberto <alberto.briano@outlook.com>
2026-06-15 23:30:37 +0200

    aaaa

--- Windows/AttackPS.ps1
+++ Windows/AttackPS.ps1
@@ -1,4 +1,3 @@
-#Invoke-WebRequest https://filegen.fortinet.com/v1/sandbox-file?file_name=windows.exe -UserAgent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36'
 $down = New-Object System.Net.WebClient
 $url = 'https://filegen.fortinet.com/v1/sandbox-file?file_name=windows.exe';
 $file = 'windows.exe';
--- Windows/Download.ps1
+++ Windows/Download.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri "https://filegen.fortinet.com/v1/sandbox-file?file_name=windows.exe" -OutFile "windows.exe"
+Invoke-WebRequest -Uri "https://filegen.fortinet.com/v1/sandbox-file?file_name=windows.exe" -OutFile "C:\windows.exe"
