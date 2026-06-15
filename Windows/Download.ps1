0471fe2fc1e1e018a05a521cb252adbe0bca8281
Alberto <alberto.briano@outlook.com>
2026-06-15 23:43:12 +0200

    Update Download.ps1

--- Windows/Download.ps1
+++ Windows/Download.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri "https://filegen.fortinet.com/v1/sandbox-file?file_name=windows.exe" -OutFile "%USERPROFILE%\windows.exe"
+Invoke-WebRequest -Uri "https://filegen.fortinet.com/v1/sandbox-file?file_name=windows.exe" -OutFile "%APPDATA%\windows.exe"
